f85e8d0ceef1859c79342fe59f748814f671deec
T3sh00g0 <146989222+T3sh00g0@users.noreply.github.com>
2025-12-23 23:18:48 -0300

    Update API server and socket IO URLs for local development

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
-window.IP_SERVER = "wppconnect-server-production-d24e.up.railway.app/api/";
-window.IP_SOCKET_IO = "wppconnect-server-production-d24e.up.railway.app/api";
+window.IP_SERVER = "http://localhost:21465//api/";
+window.IP_SOCKET_IO = "http://localhost:21465//api";
